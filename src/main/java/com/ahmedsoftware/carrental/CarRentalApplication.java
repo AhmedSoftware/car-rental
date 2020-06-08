a344bd3fa297f7ab7f184ad36615b5dcbfdd6c7f
aali <aali@osaxis.fr>
2020-06-09 00:30:31 +0200

    microservice complet avec un bus de message

--- src/main/java/com/ahmedsoftware/carrental/CarRentalApplication.java
+++ src/main/java/com/ahmedsoftware/carrental/CarRentalApplication.java
@@ -1,9 +1,12 @@
 package com.ahmedsoftware.carrental;
 
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.context.annotation.Bean;
 
 @EnableBinding(Sink.class)
 @SpringBootApplication
@@ -12,5 +15,14 @@ public class CarRentalApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CarRentalApplication.class, args);
 	}
+	/*@Bean
+	public ConnectionFactory connectionFactory() {
+		CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
+		connectionFactory.setHost("localhost");
+		connectionFactory.setVirtualHost("localhost");
+		connectionFactory.setUsername("admin");
+		connectionFactory.setPassword("admin");
+		return connectionFactory;
+	}*/
 
 }
